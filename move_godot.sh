b6a2aa1e60f5f90522387c08427193bd339a3c6a
Bryan Christopher Maass <bryan.maass@gmail.com>
2020-09-25 16:52:03 -0500

    no-build-hi

---
+++ move_godot.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+echo "moving godot releases..."
+cp ~/dv/coop_platforming/out/mac/evermeet.dmg evermeet.dmg
+cp ~/dv/coop_platforming/out/win/evermeet.exe evermeet.exe
+cp ~/dv/coop_platforming/out/linux/evermeet.x86_64 evermeet.x86_64
--- no_build_release.sh
+++ no_build_release.sh
@@ -1,1 +1,1 @@
-./link_games.joke && git add -A && git commit -am no-build-hi && git push
+./move_godot.sh && ./link_games.joke && git add -A && git commit -am no-build-hi && git push
