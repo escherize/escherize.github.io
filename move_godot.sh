7fa65c6b50a106a3303434b5daa3ca8b7b99c88b
Bryan Christopher Maass <bryan.maass@gmail.com>
2020-10-02 12:48:39 -0500

    no-build-hi

--- move_godot.sh
+++ move_godot.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 echo "moving godot releases..."
 cp ~/dv/coop_platforming/out/mac/evermeet.dmg evermeet.dmg
-cp ~/dv/coop_platforming/out/win/evermeet.exe evermeet.exe
-cp ~/dv/coop_platforming/out/win/evermeet.pck evermeet.pck
-cp ~/dv/coop_platforming/out/linux/evermeet.x86_64 evermeet.x86_64
+
+cp ~/dv/coop_platforming/out/linux/evermeet.x86_64 evermeet_linux.x86_64
+cp ~/dv/coop_platforming/out/linux/evermeet.pck evermeet_linux.pck
+zip -m evermeet_linux.zip evermeet_linux.x86_64 evermeet_linux.pck
+
+cp ~/dv/coop_platforming/out/win/evermeet.exe evermeet_win.exe
+cp ~/dv/coop_platforming/out/win/evermeet.pck evermeet_win.pck
+zip -m evermeet_win.zip evermeet_win.exe evermeet_win.pck
