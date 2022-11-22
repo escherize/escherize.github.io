c0d9fcbfac920ecfd8b62158b62b87d939d9cb32
Bryan Maass <bryan.maass@gmail.com>
2022-11-22 00:02:17 -0700

    commited at:Tue Nov 22 00:02:17 MST 2022

--- middleton/baptist/convert.clj
+++ middleton/baptist/convert.clj
@@ -12,22 +12,24 @@
           [:meta {:charset "UTF-8"}]
           [:script {:src "https://cdn.tailwindcss.com"}]]
          [:div {:style "margin:80px"}
-          [:h1.text-3xl.font-bold "Middlton First Baptist Men's Group Listing"]
+          [:h1.text-3xl.font-bold "Middlton First Baptist Men's Group List"]
           (let [[headers & rows] (with-open [reader (io/reader "mens_group_sorted.csv")]
                                    (doall
                                      (csv/read-csv reader)))]
-            [:table.min-w-full {:style "margin-top:50px"}
-             [:thead.border-b (into [:tr]
-                                    (mapv (fn [h]
-                                            [:th.text-lg.font-medium.px-6.py-4.text-left h])
-                                          headers))]
+            [:table {:style "margin-top:50px"}
+             [:thead.border-solid.border-2.border-sky-500.hover:outline-blue-500
+              (into [:tr]
+                    (mapv (fn [h]
+                            [:th.text-lg.font-medium.px-6.py-4.bg-primary.text-left h])
+                          headers))]
              (into [:tbody]
                    (mapv (fn [[f l e p]]
-                           [:tr.border-b
-                            [:td.px-6.py-4.whitespace-nowrap f]
-                            [:td.px-6.py-4.whitespace-nowrap l]
-                            [:td.px-6.py-4.whitespace-nowrap e]
-                            [:td.px-6.py-4.whitespace-nowrap p]])
+                           [:tr.border-solid.border-2.border-sky-500.hover:border-amber-500
+                            {:style "margin-top:2px"}
+                            [:td.px-4.py-3.whitespace-nowrap f]
+                            [:td.px-4.py-3.whitespace-nowrap l]
+                            [:td.px-4.py-3.whitespace-nowrap e]
+                            [:td.px-4.py-3.whitespace-nowrap p]])
                          rows))])
           [:div.pt-6.md:p-8.text-center.md:text-left.space-y-4 {:style "margin:30px"}
            [:p "Need to update your information? "
