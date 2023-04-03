7fe6408b85f6e9ffb544bb3be2f0a89b8a604f48
Bryan Maass <bryan.maass@gmail.com>
2023-04-03 09:44:21 -0600

    commited at:Mon Apr  3 09:44:20 MDT 2023

--- middleton/baptist/convert.clj
+++ middleton/baptist/convert.clj
@@ -2,7 +2,15 @@
   (:require [hiccup.core :as h]
             [clojure.data.csv :as csv]
             [clojure.java.io :as io]
-            [clojure.java.shell :as sh]))
+            [clojure.java.shell :as sh]
+            [clojure.string :as str]))
+
+;; html:
+
+
+
+
+
 
 
 (defn make-table []
@@ -35,3 +43,22 @@
           [:h1.text-3xl.font-bold "Middlton First Baptist Men's Group List"]
           (make-table)
           (footer)]]))
+
+
+;;; csv
+;;;
+;;;
+(def headers (str/split "Name,Given Name,Additional Name,Family Name,Yomi Name,Given Name Yomi,Additional Name Yomi,Family Name Yomi,Name Prefix,Name Suffix,Initials,Nickname,Short Name,Maiden Name,Birthday,Gender,Location,Billing Information,Directory Server,Mileage,Occupation,Hobby,Sensitivity,Priority,Subject,Notes,Language,Photo,Group Membership,E-mail 1 - Type,E-mail 1 - Value,E-mail 2 - Type,E-mail 2 - Value,Phone 1 - Type,Phone 1 - Value,Address 1 - Type,Address 1 - Formatted,Address 1 - Street,Address 1 - City,Address 1 - PO Box,Address 1 - Region,Address 1 - Postal Code,Address 1 - Country,Address 1 - Extended Address,Website 1 - Type,Website 1 - Value" #","))
+
+(def old->new
+  {"LastName" "Family Name"
+   "FirstName" "Given Name"
+   "email" "E-mail 1 - Value"
+   "phone" "Phone 1 - Value"})
+
+(spit "middleton/baptist/new.csv"
+      (let [[headers & rows] (with-open [reader (io/reader "middleton/baptist/mens_group_sorted.csv")]
+                               (doall (csv/read-csv reader)))
+            new-headers (mapv old->new headers)]
+        (str/join "\n"
+                  (concat [(str/join "," new-headers)] (mapv #(str/join "," %) rows)))))
