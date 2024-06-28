28e2b0e74df693ce7107243154a32759f45ae676
Bryan Maass <bryan.maass@gmail.com>
2024-06-27 20:05:18 -0600

    add service worker

---
+++ works/rp/service-worker.js
@@ -0,0 +1,24 @@
+// service-worker.js
+   self.addEventListener('install', function(event) {
+     self.skipWaiting();
+   });
+
+   self.addEventListener('activate', function(event) {
+     event.waitUntil(self.clients.claim());
+   });
+
+   self.addEventListener('fetch', function(event) {
+     event.respondWith(
+       fetch(event.request).then(function(response) {
+         const newHeaders = new Headers(response.headers);
+         newHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
+         newHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
+
+         return new Response(response.body, {
+           status: response.status,
+           statusText: response.statusText,
+           headers: newHeaders
+         });
+       })
+     );
+   });
