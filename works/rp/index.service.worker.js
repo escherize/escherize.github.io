4685a2c68ceb710fa43b16c02a5430eddc1118ac
Bryan Maass <bryan.maass@gmail.com>
2024-07-01 22:57:59 -0600

    commited at:Mon Jul  1 22:57:58 MDT 2024

--- works/rp/index.service.worker.js
+++ works/rp/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1719587029|18734353501";
+const CACHE_VERSION = "1719896265|4977515448";
 const CACHE_PREFIX = "roguelite_platfo-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
